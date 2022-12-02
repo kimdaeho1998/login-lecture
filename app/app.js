9b8801dd59c26ed603bc4c23a69bec165b96dc29
kimdaeho <kimdaeho1998@naver.com>
2022-12-02 15:43:27 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,16 @@
+//module
+const express = require("express");
+const app = express();
+
+
+// routing
+const home = require("./src/routes/home");
+
+// 앱 세팅
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+
+
+app.use("/", home); // use -> middle ware 를 등록 해주는 메소드
+
+module.exports = app;
---
+++ app/src/routes/home/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+
+const ctr = require("./home.ctr")
+
+router.get("/", ctr.home);
+
+router.get("/login", ctr.login);
+
+module.exports = router;
+
