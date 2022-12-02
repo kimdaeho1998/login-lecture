972f1791e8673112a1cd1ec1080a710cadfde92d
kimdaeho <kimdaeho1998@naver.com>
2022-12-02 16:57:14 +0900

    돔을 이용하여 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const id = document.querySelector("#id"),
+      pwd = document.querySelector("#pwd"),
+      loginbtn = document.querySelector("button");
+
+loginbtn.addEventListener("click", login);
+
+function login()
+{
+    const req = {
+        id : id.value,
+        pwd : pwd.value,
+    };
+
+    console.log(req);
+}
