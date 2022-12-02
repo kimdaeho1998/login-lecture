f133c3131c46ec3132aa191973bc0bcf1244e577
kimdaeho <kimdaeho1998@naver.com>
2022-12-02 16:07:49 +0900

    프런트 단 js 파일 만들기

--- app/app.js
+++ app/app.js
@@ -9,7 +9,7 @@ const home = require("./src/routes/home");
 // 앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use -> middle ware 를 등록 해주는 메소드
 
